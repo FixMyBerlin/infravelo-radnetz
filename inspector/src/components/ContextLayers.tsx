6103556e36fc2402dfa1b6984d3cada8cbb2d374
Henri <henri@fixmycity.de>
2025-10-09 18:58:36 +0200

    Inspector: Bezirksgrenzen on top of all

--- inspector/src/components/ContextLayers.tsx
+++ inspector/src/components/ContextLayers.tsx
@@ -87,7 +87,10 @@ export const ContextMapLayers = () => {
       {/* Marker layer to ensure context layers are on top and to allow beforeId references */}
       <Layer id="static-layers-start" type="background" paint={{ 'background-opacity': 0 }} />
 
-      {/* Vorrangnetz (mask) */}
+      {/* Marker layer to position Vorrangnetz below Bezirke */}
+      <Layer id="bezirke-layers-start" type="background" paint={{ 'background-opacity': 0 }} />
+
+      {/* Vorrangnetz (mask) - always positioned before bezirke-layers-start */}
       {contextLayers.includes('vorrangnetz') && (
         <Fragment>
           <Source
@@ -107,6 +110,7 @@ export const ContextMapLayers = () => {
               'line-opacity': 0.8,
               'line-offset': ['interpolate', ['linear'], ['zoom'], 0, 0, 12.5, 0, 13, -0.5],
             }}
+            beforeId="bezirke-layers-start"
           />
           <Layer
             id="vorrangnetz-mask-fill"
@@ -118,6 +122,56 @@ export const ContextMapLayers = () => {
               'fill-color': '#27272a',
               'fill-opacity': ['interpolate', ['linear'], ['zoom'], 0, 0.99, 13.5, 0.95, 14, 0.7],
             }}
+            beforeId="bezirke-layers-start"
+          />
+        </Fragment>
+      )}
+
+      {/* Bezirke */}
+      {contextLayers.includes('bezirke') && (
+        <Fragment>
+          <Source
+            id="context-bezirke"
+            type="vector"
+            url="pmtiles://https://tilda-geo.de/api/uploads/berlin-bezirke"
+          />
+          <Layer
+            id="context-bezirke-border"
+            type="line"
+            source="context-bezirke"
+            filter={['==', '$type', 'Polygon']}
+            paint={{
+              'line-width': 6,
+              'line-color': '#fbbf24',
+              'line-opacity': 0.9,
+              'line-offset': -3,
+            }}
+            source-layer="default"
+          />
+          <Layer
+            id="context-bezirke-label"
+            type="symbol"
+            source="context-bezirke"
+            paint={{
+              'text-color': '#fffbeb',
+              'text-halo-color': '#fbbf24',
+              'text-halo-width': 3,
+              'text-opacity': 1,
+            }}
+            layout={{
+              'text-line-height': 1.1,
+              'text-size': 14,
+              'text-radial-offset': 0,
+              'text-allow-overlap': true,
+              'symbol-avoid-edges': true,
+              'text-ignore-placement': true,
+              'symbol-placement': 'line',
+              'text-justify': 'auto',
+              'text-padding': 0,
+              'text-offset': [0, 0.7],
+              'text-field': ['concat', ['to-string', ['get', 'Gemeinde_name']]],
+            }}
+            source-layer="default"
           />
         </Fragment>
       )}
@@ -186,65 +240,6 @@ export const ContextMapLayers = () => {
 
       {/* Mapillary FMC Befahrung */}
       {contextLayers.includes('mapillary_fmc') && <MapillaryFMCLayers />}
-
-      {/* Bezirke */}
-      {contextLayers.includes('bezirke') && (
-        <Fragment>
-          <Source
-            id="context-bezirke"
-            type="vector"
-            url="pmtiles://https://tilda-geo.de/api/uploads/berlin-bezirke"
-          />
-          <Layer
-            id="context-bezirke-border"
-            type="line"
-            source="context-bezirke"
-            filter={['==', '$type', 'Polygon']}
-            paint={{
-              'line-width': 6,
-              'line-color': '#fbbf24',
-              'line-opacity': 0.9,
-              'line-offset': -3,
-            }}
-            source-layer="default"
-            beforeId={
-              contextLayers.includes('vorrangnetz')
-                ? 'vorrangnetz-mask-fill'
-                : 'static-layers-start'
-            }
-          />
-          <Layer
-            id="context-bezirke-label"
-            type="symbol"
-            source="context-bezirke"
-            paint={{
-              'text-color': '#fffbeb',
-              'text-halo-color': '#fbbf24',
-              'text-halo-width': 3,
-              'text-opacity': 1,
-            }}
-            layout={{
-              'text-line-height': 1.1,
-              'text-size': 14,
-              'text-radial-offset': 0,
-              'text-allow-overlap': true,
-              'symbol-avoid-edges': true,
-              'text-ignore-placement': true,
-              'symbol-placement': 'line',
-              'text-justify': 'auto',
-              'text-padding': 0,
-              'text-offset': [0, 0.7],
-              'text-field': ['concat', ['to-string', ['get', 'Gemeinde_name']]],
-            }}
-            source-layer="default"
-            beforeId={
-              contextLayers.includes('vorrangnetz')
-                ? 'vorrangnetz-mask-fill'
-                : 'static-layers-start'
-            }
-          />
-        </Fragment>
-      )}
     </Fragment>
   )
 }
