19d6459c65467396685be3ff968ae8a16cc2ec65
Henri <henri@fixmycity.de>
2025-11-13 11:41:57 +0100

    Inspector: Layer TODO improved

--- inspector/src/components/shared/widthStyle.ts
+++ inspector/src/components/shared/widthStyle.ts
@@ -4,23 +4,13 @@ import type { LayerLegend } from './types'
 // Color constants
 export const WIDTH_COMPLETE = '#32CD32' // green - width and source present
 export const WIDTH_MISSING = '#FF0000' // red - width missing
-export const WIDTH_MISSING_LOW_PRIORITY = '#FFE4E1' // very light red - width missing but not priority
 export const WIDTH_SOURCE_MISSING = '#914ec7ff' // dark red - width present but source missing
 
 export const getWidthOpacity: DataDrivenPropertyValueSpecification<number> = 0.8
 
 export const getWidthStyle: DataDrivenPropertyValueSpecification<string> = [
   'case',
-  // First check if it's a category where width is not a priority
-  [
-    'any',
-    ['==', ['get', 'category'], 'sharedBusLaneBusWithBike'],
-    ['==', ['get', 'category'], 'sharedBusLaneBikeWithBus'],
-    ['==', ['get', 'category'], 'bicycleRoad'],
-    ['==', ['get', 'category'], 'bicycleRoad_vehicleDestination'],
-  ],
-  WIDTH_MISSING_LOW_PRIORITY,
-  // Then check if width is completely missing
+  // Check if width is completely missing
   ['!', ['has', 'width']],
   WIDTH_MISSING,
   // Then check if width source is missing
@@ -35,6 +25,5 @@ export const getWidthLegend = (): LayerLegend => ({
     { color: WIDTH_COMPLETE, label: 'Breite und Quelle vorhanden' },
     { color: WIDTH_SOURCE_MISSING, label: '[TODO] Quellenangabe der Breite fehlt' },
     { color: WIDTH_MISSING, label: '[TODO] Breitenangeabe fehlt' },
-    { color: WIDTH_MISSING_LOW_PRIORITY, label: 'Breitenangabe fehlt aber auch keine Priorität' },
   ],
 })
